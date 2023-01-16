14dc211a50055979070f07c1ef70c94723b10ccc
satyaindumathi <burrisatya2003@gmail.com>
2023-01-16 21:56:46 +0530

    errors fixed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 var csrf = require("tiny-csrf");
 var cookieParser = require("cookie-parser");
 const app = express();
-const { Admin, Election,Question,Option,Voter } = require("./models");
+const { Admin, Election, Question, Option, Voter } = require("./models");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const connectEnsureLogin = require("connect-ensure-login");
@@ -83,7 +83,8 @@ passport.deserializeUser((id, done) => {
 app.get("/login", (request, response) => {
   response.render("login", {
     title: "Login",
-   csrfToken: request.csrfToken() });
+    csrfToken: request.csrfToken(),
+  });
 });
 app.get("/signup", (request, response) => {
   response.render("signup", {
@@ -94,9 +95,9 @@ app.get("/signup", (request, response) => {
 app.post("/admin", async (request, response) => {
   const hashedPwd = await bcrypt.hash(request.body.password, saltRounds);
   console.log(hashedPwd);
-  if(request.body.password.length<8){
-    request.flash("error","Password should contain atleast of length 10");
-    response.redirect("/signup")
+  if (request.body.password.length < 8) {
+    request.flash("error", "Password should contain atleast of length 10");
+    response.redirect("/signup");
   }
   try {
     const admin = await Admin.create({
@@ -118,36 +119,40 @@ app.post("/admin", async (request, response) => {
     return response.redirect("/signup");
   }
 });
-app.post("/Election",connectEnsureLogin.ensureLoggedIn(),async (request,response)=>{
-  if(request.body.electionName.length==0){
-    request.flash("error","Election Name must be specified")
-    return response.redirect("/Election")
+app.post(
+  "/Election",
+  connectEnsureLogin.ensureLoggedIn(),
+  async (request, response) => {
+    const nullString=(request.body.ElectionName).trim()
+  if(nullString.length==0){
+    request.flash("error","Election Name Should not be Null")
+    return response.redirect("/Election/Create")
   }
-  const url=request.body.CustomURL
-  function stringHasTheWhiteSpaceOrNot(value){
-    return value.indexOf(' ') >= 0;
- }
- const whiteSpace=stringHasTheWhiteSpaceOrNot(url);
- if(whiteSpace==true){
-  request.flash("error","White spaces are not allowed")
-  console.log("Spaces found")
-    return response.redirect("/Election")
- }
-
-  try{
-        await Election.addElection({
-          ElectionName:request.body.ElectionName,
-          CustomURL:request.body.CustomURL,
-          csrfToken:request.csrfToken(),
-          AdminId:request.user.id,
-        });
-       return response.redirect("/Election")
+    const url = request.body.CustomURL;
+    function stringHasTheWhiteSpaceOrNot(value) {
+      return value.indexOf(" ") >= 0;
     }
-    catch (error) {
+    const haswhiteSpace = stringHasTheWhiteSpaceOrNot(url);
+    if (haswhiteSpace == true) {
+      request.flash("error", "White spaces are not allowed");
+      console.log("Spaces found");
+      return response.redirect("/Election/Create");
+    }
+
+    try {
+      await Election.addElection({
+        ElectionName: request.body.ElectionName,
+        CustomURL: request.body.CustomURL,
+        csrfToken: request.csrfToken(),
+        AdminId: request.user.id,
+      });
+      return response.redirect("/Election");
+    } catch (error) {
       request.flash("error", error.message);
       return response.redirect("/Election");
     }
-})
+  }
+);
 app.post(
   "/session",
   passport.authenticate("local", {
@@ -180,84 +185,101 @@ app.get(
       console.log(error);
       return response.status(422).json(error);
     }
-  },
+  }
 ),
-
-  app.get("/elections/:id",connectEnsureLogin.ensureLoggedIn(),async(request,response)=>{
-    const election= await Election.getElectionWithId(request.params.id);
-    const questionsCount=await Question.countOFQuestions(request.params.id);
-    const votersCount=await Voter.countOFVoters(request.params.id);
-    console.log(questionsCount)
-   return response.render("Question",{
-      id:request.params.id,
-      title:election.ElectionName,
-      csrfToken:request.csrfToken(),
-      QuestionsC:questionsCount,
-      votersC:votersCount,
-      CustomURL:election.CustomURL,
-    }) 
-}),
-app.get("/Election/:id/NewQuestion",connectEnsureLogin.ensureLoggedIn(),async(request,response)=>{
-  
-    const Election=await Election.getElectionWithId(request.params.id);
-    const Question=await Question.getQuestionWithId(request.params.id);
-    if(Election.isRunning==false){
-      if(request.accepts("html")){
-        return response.render("NewQuestion",{
-          title:Election.ElectionName,
-          Question:Question,
-          csrfToken:request.csrfToken(),
-          id:request.params.id,
-        })
-      }
-      else{
-        return response.json({Question})
+  app.get(
+    "/elections/:id",
+    connectEnsureLogin.ensureLoggedIn(),
+    async (request, response) => {
+      const election = await Election.getElectionWithId(request.params.id);
+      const questionsCount = await Question.countOFQuestions(request.params.id);
+      const votersCount = await Voter.countOFVoters(request.params.id);
+      console.log(questionsCount);
+      return response.render("Question", {
+        id: request.params.id,
+        title: election.ElectionName,
+        csrfToken: request.csrfToken(),
+        QuestionsC: questionsCount,
+        votersC: votersCount,
+        CustomURL: election.CustomURL,
+      });
+    }
+  ),
+  app.get(
+    "/Election/:id/NewQuestion",
+    connectEnsureLogin.ensureLoggedIn(),
+    async (request, response) => {
+      const Election = await Election.getElectionWithId(request.params.id);
+      const Question = await Question.getQuestionWithId(request.params.id);
+      if (Election.isRunning == false) {
+        if (request.accepts("html")) {
+          return response.render("NewQuestion", {
+            title: Election.ElectionName,
+            Question: Question,
+            csrfToken: request.csrfToken(),
+            id: request.params.id,
+          });
+        } else {
+          return response.json({ Question });
+        }
+      } else {
+        request.flash(
+          "error",
+          "Cannot access questions while election is running"
+        );
+        return response.redirect(`/election/${request.params.id}`);
       }
     }
-    else{
-      request.flash("error","Cannot access questions while election is running");
-      return response.redirect(`/election/${request.params.id}`)
-    }
-  
- 
-
-});
-app.get("/Election/:id/NewQuestion/Create",connectEnsureLogin.ensureLoggedIn(),async(request,response)=>{
-  return response.render("Create-question",{
-    id: request.params.id,
-    csrfToken:request.csrfToken()
-  })
-})
-app.get("/Election/Create",connectEnsureLogin.ensureLoggedIn(),async (request,response)=>{
-  response.render("CreateElection",{
-      title:"New Election",
-      csrfToken:request.csrfToken(),
-  })
-
-})
-app.post("/Election/:id/NewQuestion/Create",connectEnsureLogin.ensureLoggedIn(),async(request,response)=>{
-  const enteredQuestion=(request.body.question).trim()
-  if(enteredQuestion.length==0){
-    request.flash("error","Question should be something not null")
-    return response.redirect(`/Election/${request.params.id}/NewQuestion/Create`)
-  }
-
-  try{
-    const question=request.body.question;
-    const description=request.body.description;
-    const electionId=request.params.id
-    await Question.addNewQuestion({
-      question,
-      description,
-      electionId,
+  );
+app.get(
+  "/Election/:id/NewQuestion/Create",
+  connectEnsureLogin.ensureLoggedIn(),
+  async (request, response) => {
+    return response.render("Create-question", {
+      id: request.params.id,
+      csrfToken: request.csrfToken(),
     });
-  return response.redirect(`/Election/${request.params.id}`)
   }
-  catch(error){
-    request.flash("error",error)
-    return response.redirect(`/Election/${request.params.id}/NewQuestion/Create`)
+);
+app.get(
+  "/Election/Create",
+  connectEnsureLogin.ensureLoggedIn(),
+  async (request, response) => {
+    response.render("CreateElection", {
+      title: "New Election",
+      csrfToken: request.csrfToken(),
+    });
+  }
+);
+app.post(
+  "/Election/:id/NewQuestion/Create",
+  connectEnsureLogin.ensureLoggedIn(),
+  async (request, response) => {
+    const enteredQuestion = request.body.question.trim();
+    if (enteredQuestion.length == 0) {
+      request.flash("error", "Question should be something not null");
+      return response.redirect(
+        `/Election/${request.params.id}/NewQuestion/Create`
+      );
+    }
 
-  } 
-})
+    try {
+      const question = request.body.question;
+      const description = request.body.description;
+      const electionId = request.params.id;
+      await Question.addNewQuestion({
+        question,
+        description,
+        electionId,
+      });
+      return response.redirect(`/Election/${request.params.id}`);
+    } catch (error) {
+      request.flash("error", error);
+      return response.redirect(
+        `/Election/${request.params.id}/NewQuestion/Create`
+      );
+    }
+  }
+);
 
 module.exports = app;
